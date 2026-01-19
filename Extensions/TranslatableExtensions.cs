70c0cd4772f3f74a6a98df1a9e9fc632ce02939e
Khalid A. Fazal <khalid@octedia.com>
2026-01-19 21:56:41 +0300

    fix: Allow NULL values in database

--- Extensions/TranslatableExtensions.cs
+++ Extensions/TranslatableExtensions.cs
@@ -90,7 +90,9 @@ namespace EFTranslatable.Extensions
                                   ? new Translatable(new Dictionary<string, string>())
                                   : new Translatable(c.Translations.ToDictionary(_ => _.Key, _ => _.Value))
                           )
-                      ).HasColumnType(columnType);
+                      )
+                      .HasColumnType(columnType)
+                      .IsRequired(false); // Allow NULL values in database - converter will handle them
                 }
             }
 
